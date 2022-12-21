9526c133d748f7eb6f3e30858d2106848be3b523
tlskals <111082777+tlskals@users.noreply.github.com>
2022-12-21 11:29:56 +0900

    feat: AnswerController 기본 구조 구현

---
+++ back/src/main/java/pre006/stackoverflow/domain/answer/AnswerController.java
@@ -0,0 +1,32 @@
+package pre006.stackoverflow.domain.answer;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+@Validated
+public class AnswerController {
+
+    @PostMapping // 답변 추가
+    public ResponseEntity postAnswer() {
+        return ResponseEntity.ok(null);
+    }
+
+    @PatchMapping // 답변 수정
+    public ResponseEntity patchAnswer() {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping // 답변 검색
+    public ResponseEntity getAnswer() {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping // 답변 삭제
+    public ResponseEntity deleteAnswer() {
+        return ResponseEntity.ok(null);
+    }
+}
