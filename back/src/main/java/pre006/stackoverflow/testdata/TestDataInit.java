132e93a11ec29488cb3f40d0ba1bb97803ac1f88
pepsi160ml <111164516+pepsi160ml@users.noreply.github.com>
2023-01-02 11:59:12 +0900

    fix dg
    dg

--- back/src/main/java/pre006/stackoverflow/testdata/TestDataInit.java
+++ back/src/main/java/pre006/stackoverflow/testdata/TestDataInit.java
@@ -49,6 +49,7 @@ public class TestDataInit {
         userController.post(new UserPostDto("hgd5@gmail.com", "박자바", "3333",
                 "저는 김영희 입니다.", "천안"));
 
+<<<<<<< Updated upstream
         // 태그 등록
         tagController.postTag(new TagDto.TagPostDto("java"));
         tagController.postTag(new TagDto.TagPostDto("spring"));
@@ -126,5 +127,78 @@ public class TestDataInit {
 //                new CommentPostDto("김코딩의 1번째 질문에 대한 1번째 답변의 2번째 코멘트입니다."));
 //    }
 //
+=======
+        // 질문 등록
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (1L, "홍길동의 1번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (1L, "홍길동의 2번째 질문입니다.", "두번째 질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (2L, "김코딩의 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (3L, "박자바의 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (3L, "박자바의 2번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (2L, "김코딩의 2번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (2L, "김코딩의 3번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (2L, "김코딩의 4번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (3L, "박자바의 2번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (3L, "박자바의 3번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (4L, "김철수의 1번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (4L, "김철수의 2번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (5L, "김영희의 1번째 질문입니다.", "질문 내용입니다."));
+//        questionController.postQuestion(new QuestionDto.QuestionPostDto
+//                (5L, "김영희의 2번째 질문입니다.", "질문 내용입니다."));
+
+        // 답변 등록
+        answerController.postAnswer(1L, 2L,
+                new AnswerPostDto("홍길동의 1번째 질문에 대한 답변입니다."));
+        answerController.postAnswer(1L, 3L,
+                new AnswerPostDto("홍길동의 1번째 질문에 대한 2번째 답변입니다."));
+        answerController.postAnswer(2L, 2L,
+                new AnswerPostDto("홍길동의 2번째 질문에 대한 답변입니다."));
+        answerController.postAnswer(3L, 1L,
+                new AnswerPostDto("김코딩의 질문에 대한 답변입니다."));
+        answerController.postAnswer(3L, 3L,
+                new AnswerPostDto("김코딩의 질문에 대한 2번째 답변입니다."));
+        answerController.postAnswer(4L, 2L,
+                new AnswerPostDto("박자바의 질문에 대한 답변입니다."));
+
+        // 질문 댓글 등록
+        commentController.postQuestionComment(1L, 1L,
+                new CommentPostDto("홍길동의 1번째 질문에 대한 1번째 코멘트입니다."));
+        commentController.postQuestionComment(2L, 1L,
+                new CommentPostDto("홍길동의 1번째 질문에 대한 2번째 코멘트입니다."));
+        commentController.postQuestionComment(3L, 1L,
+                new CommentPostDto("홍길동의 1번째 질문에 대한 3번째 코멘트입니다."));
+        commentController.postQuestionComment(1L, 2L,
+                new CommentPostDto("홍길동의 2번째 질문에 대한 1번째 코멘트입니다."));
+        commentController.postQuestionComment(1L, 3L,
+                new CommentPostDto("김코딩의 질문에 대한 1번째 코멘트입니다."));
+        commentController.postQuestionComment(3L, 3L,
+                new CommentPostDto("김코딩의 질문에 대한 2번째 코멘트입니다."));
+
+        // 답변 댓글 등록
+        commentController.postAnswerComment(1L, 1L,
+                new CommentPostDto("홍길동의 1번째 질문에 대한 1번째 답변의 1번째 코멘트입니다."));
+        commentController.postAnswerComment(2L, 1L,
+                new CommentPostDto("홍길동의 1번째 질문에 대한 1번째 답변의 2번째 코멘트입니다."));
+        commentController.postAnswerComment(3L, 1L,
+                new CommentPostDto("홍길동의 1번째 질문에 대한 1번째 답변의 3번째 코멘트입니다."));
+        commentController.postAnswerComment(3L, 2L,
+                new CommentPostDto("홍길동의 1번째 질문에 대한 2번째 답변의 1번째 코멘트입니다."));
+        commentController.postAnswerComment(3L, 4L,
+                new CommentPostDto("김코딩의 1번째 질문에 대한 1번째 답변의 1번째 코멘트입니다."));
+        commentController.postAnswerComment(2L, 4L,
+                new CommentPostDto("김코딩의 1번째 질문에 대한 1번째 답변의 2번째 코멘트입니다."));
+>>>>>>> Stashed changes
     }
 }
