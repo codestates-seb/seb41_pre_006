59bc344b2d6012eb750b792d8f2ab4a70d5d98e4
tlskals <111082777+tlskals@users.noreply.github.com>
2023-01-02 11:59:01 +0900

    fear: application.properties mysql 정의

--- back/src/main/java/pre006/stackoverflow/domain/auth/jwt/JwtTokenizer.java
+++ back/src/main/java/pre006/stackoverflow/domain/auth/jwt/JwtTokenizer.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
